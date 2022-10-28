f03123f45e8de811eb6440a088cc70ed14524a9e
Carlos Pavajeau <cantte098@gmail.com>
2022-10-28 11:06:08 -0500

    feat(shared): add mongo collection builder

---
+++ src/Shared/Domain/Persistence/IMongoCollectionBuilder.cs
@@ -0,0 +1,8 @@
+﻿using MongoDB.Driver;
+
+namespace Dew.Shared.Domain.Persistence;
+
+public interface IMongoCollectionBuilder
+{
+    IMongoCollection<TCollection> Build<TCollection>();
+}
---
+++ src/Shared/Infrastructure/Persistence/MongoCollectionBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Dew.Shared.Domain.Persistence;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Dew.Shared.Infrastructure.Persistence;
+
+public class MongoCollectionBuilder : IMongoCollectionBuilder
+{
+    private readonly IMongoDatabase _database;
+    private readonly MongoDbSettings _settings;
+
+    public MongoCollectionBuilder(IOptions<MongoDbSettings> options)
+    {
+        _settings = options.Value;
+
+        var client = new MongoClient(_settings.ConnectionString);
+        _database = client.GetDatabase(_settings.DatabaseName);
+    }
+
+    public IMongoCollection<TCollection> Build<TCollection>() =>
+        _database.GetCollection<TCollection>(_settings.CollectionName);
+}
