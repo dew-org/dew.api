a8c9e25a56eb608c2cf4a4e80343a357607ab4db
Carlos Pavajeau <cantte098@gmail.com>
2022-11-08 12:11:33 -0500

    feat(catalogue): add create product command validator
    See: DEW-T-1
    Signed-off-by: Carlos Pavajeau <cantte098@gmail.com>

---
+++ src/Catalogue/Application/Create/CreateProductCommandValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+
+namespace Dew.Catalogue.Application.Create;
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Code)
+            .NotEmpty()
+            .MaximumLength(50);
+
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(x => x.Description)
+            .MaximumLength(500);
+
+        RuleFor(x => x.Price).SetValidator(new CreateProductPriceValidator());
+
+        RuleFor(x => x.Discount)
+            .GreaterThan(0)
+            .LessThan(1);
+
+        RuleFor(x => x.Tax)
+            .GreaterThan(0)
+            .LessThan(1);
+
+        RuleFor(x => x.UserId)
+            .NotEmpty();
+    }
+}
---
+++ src/Catalogue/Application/Create/CreateProductPriceValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Dew.Catalogue.Application.Create;
+
+public class CreateProductPriceValidator : AbstractValidator<CreateProductPrice>
+{
+    public CreateProductPriceValidator()
+    {
+        RuleFor(x => x.RetailPrice)
+            .NotEmpty()
+            .GreaterThan(0);
+
+        RuleFor(x => x.SalePrice)
+            .NotEmpty()
+            .GreaterThan(0);
+
+        RuleFor(x => x.Currency)
+            .NotEmpty();
+    }
+}
